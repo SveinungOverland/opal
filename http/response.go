668f551a91076fa87f40c23b7fd9669592694f84
Anders Hallem Iversen <andershallemiversen@hotmail.com>
2019-04-25 23:28:49 +0200

    Added relevant response methods

--- http/response.go
+++ http/response.go
@@ -1,15 +1,88 @@
 package http
 
+import (
+	"encoding/json"
+	"io/ioutil"
+	"html/template"
+	"bytes"
+	"fmt"
+)
+
 type Response struct {
 	Status uint16
 	Body   []byte
 	Header map[string]string
 }
 
-func (r *Response) NotFound() {
-	r.Status = 404
+// ----- BODY MODIFIERS -----
+
+// JSON sets body to the given content in json-format 
+func (res *Response) JSON(target interface{}) {
+	res.Header["content-type"] = "application/json"
+	json, _ := json.Marshal(target)
+
+	res.Body = json
 }
 
+// File reads and sets the file
+func (res *Response) File(path string, contentType string) {
+	file, err := ioutil.ReadFile(path)
+	if err != nil {
+		return
+	}
+
+	res.Body = file
+	res.Header["Content-Type"] = contentType
+}
+
+// HTML reads an html-file and builds a html-response. Includes support for templates.
+func (res *Response) HTML(path string, templateData interface{}) {
+	temp, err := template.ParseFiles(path)
+	if err != nil {
+		fmt.Println("Error: ", err)
+	}
+
+	var tpl bytes.Buffer
+	if err := temp.Execute(&tpl, templateData); err != nil {
+		return
+	}
+	res.Body = tpl.Bytes()
+	res.Header["content-type"] = "text/html; charset=utf-8"
+}
+
+// ----- STATUS SETTERS ------
+
+// Ok sets response status-code to 200
+func (res *Response) Ok() {
+	res.Status = 200
+}
+
+// Created sets response status-code to 201
+func (res *Response) Created() {
+	res.Status = 201
+}
+
+// BadRequest sets response status-code to 403
+func (res *Response) BadRequest() {
+	res.Status = 400
+}
+
+// Unauthorized sets response status-code to 401
+func (res *Response) Unauthorized() {
+	res.Status = 401
+}
+
+// Forbidden sets response status-code to 403
+func (res *Response) Forbidden() {
+	res.Status = 403
+}
+
+// NotFound sets response status-code to 404
+func (res *Response) NotFound() {
+	res.Status = 404
+}
+
+// ------ RESPONSE BUILDERS ------
 
 func NewResponse() *Response {
 	res := &Response{

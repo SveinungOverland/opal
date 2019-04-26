461ab094730ffad2d3d7fefcf3a81faeda19794c
Anders Hallem Iversen <andershallemiversen@hotmail.com>
2019-04-26 02:06:50 +0200

    Tested pusher

--- example/server.go
+++ example/server.go
@@ -26,10 +26,15 @@ func main() {
 		res.Body = []byte("Hello World")
 	})
 
-	r.Get("/site", func(req *http.Request, res *http.Response) {
-		res.Body = []byte("<html><body><h4>Hello World! :D</h4><a href=\"\\\">Click here</a></body></html>")	
+	r.Get("/site", func(req *opalHttp.Request, res *opalHttp.Response) {
+		res.Body = []byte("<html><head><link rel=\"stylesheet\" type=\"text/css\" href=\"css\\theme.css\"></head><body><h4>Hello World! :D</h4><a href=\"\\\">Click here</a></body></html>")	
 		res.Header["content-type"] = "text/html; charset=utf-8"
+
+		/* pushClient := opalHttp.NewPusher(req, res)
+		pushClient.Push("/css/theme.css") */
 	})
+
+	r.Static("/css", "./css")
 
 	srv.Register(r)
 
